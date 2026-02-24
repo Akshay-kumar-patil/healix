e1f2447ed16a3fa66d5ffbbdc00414d23d8b56af
Akshay Kumar Patil <akshaykumarpatil58@gmail.com>
2026-02-24 13:16:36 +0530

    config

--- app/config.py
+++ app/config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 class Config:
     
-    MODEL_NAME="model/gemini-2.5-flash"
+    MODEL_NAME="gemini-2.5-flash"
 
     EMBEDDING_MODEL='sentence-transformers/all-MiniLM-L6-v2'
 
@@ -16,7 +16,7 @@ class Config:
     MODEL_CACHE_DIR = "./models"
 
     # retrival 
-    CHROMA_DIR="vectore_store"
+    CHROMA_DIR = "vector_store"
     SEARCH_TYPE="similarity"
     CROSS_ENCODER_MODEL="cross-encoder/ms-marco-MiniLM-L-6-v2"
     # reliability setting
@@ -24,11 +24,10 @@ class Config:
     SIMILARITY_THRESHOLD =0.75
 
     GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-    GEMINI_MODEL = "gemini-2.0-flash-exp"
+    GEMINI_MODEL = "gemini-2.5-flash"
     TEMPERATURE=0.3
     MAX_TOKENS=1500
 
-    MAX_RETRIES = 2 
     QUALITY_THRESHOLD = 0.3 
 
     # Orchestrator settings
